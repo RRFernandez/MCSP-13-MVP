5892da805c75b80f9997d5e1f7af25de6fd18899
RRFernandez <98431164+RRFernandez@users.noreply.github.com>
2022-08-03 23:57:44 -0400

    Almost done. Need to finish fetch delete.

--- server.js
+++ server.js
@@ -18,28 +18,84 @@ const dbConfig = {
 };
 
 if (process.env.NODE_ENV === "production") {
-    dbConfig.ss = {
+    dbConfig.ssl = {
         rejectUnauthorized: false
     };
 }
 
 const pool = new pg.Pool(dbConfig);
 
-app.get("/todo", (req, res, next) => {
-    pool.query('SELECT * FROM lists').then((data) => {
-            console.log(data);
-            res.send(data.rows);
-        })
-        .catch(next)
-});
+const unknownHTTP = (req, res, next) => {
+    res.sendStatus(404);
+    next();
+};
 
-app.get("/todo/tasks", (req, res, next) => {
+app.get("/todo", (req, res, next) => {
     pool.query('SELECT * FROM tasks').then((data) => {
             console.log(data);
             res.send(data.rows);
         })
         .catch(next)
 });
+
+// app.get("/todo/tasks", (req, res, next) => {
+//     pool.query('SELECT * FROM tasks').then((data) => {
+//             console.log(data);
+//             res.send(data.rows);
+//         })
+//         .catch(next)
+// });
+
+// app.get("/todo/:id", (req, res, next) => {
+//     pool.query('SELECT * FROM lists WHERE list_id = $1', [id]).then((data) => {
+//             if (data.rows[0]) {
+//                 res.send(data.rows[0]);
+//             } else {
+//                 res.sendStatus(404);
+//             }
+//         })
+//         .catch(next)
+// });
+
+
+// app.get("/todo/tasks/:id", (req, res, next) => {
+//     const id = req.params.id;
+//     pool.query('SELECT * FROM tasks WHERE list_id = $1', [id]).then((data) => {
+//             if (data.rows[0]) {
+//                 res.send(data.rows[0]);
+//             } else {
+//                 res.sendStatus(404);
+//             }
+//         })
+//         .catch(next)
+// });
+
+app.post("/todo", (req, res, next) => {
+    const { name, list_id } = req.body;
+    pool.query('INSERT INTO tasks (name, list_id) VALUES ($1, $2) RETURNING *;', [name, list_id])
+        .then((data) => {
+            res.send(data.rows[0])
+        })
+        .catch(next)
+})
+
+app.delete("/todo", (req, res, next) => {
+    const { name } = req.body;
+    pool.query('DELETE FROM tasks WHERE name = $1 RETURNING *;', [name])
+        .then((data) => {
+            console.log(data.rows[0])
+            console.log("DELETE REQ")
+            res.sendStatus(204);
+        })
+        .catch(next);
+})
+
+app.use(unknownHTTP);
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.sendStatus(500)
+})
 
 app.listen(PORT, () => {
     console.log(`Listening on ${PORT}`);
--- static/app.js
+++ static/app.js
@@ -1,9 +1,115 @@
-const $list = $("#list");
-const $task = $("#task");
+//selectors
+const $todoList = $("#todo-list");
+const $todoBtn = $(".todo-button")
+const $todoInput = $(".todo-input")
+const $filterOption = $(".filter-todo")
 
-fetch("/todo")
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-        $list.append(JSON.stringify(data))
-    });
+
+//functions
+
+const createTodo = (data) => {
+    for (let i = 0; i < data.length; i++) {
+        const $todoDiv = $('<div></div>').addClass("todo-div");
+        const $newTask = $('<li></li>').addClass("task");
+        $todoDiv.append($newTask);
+        const $completeBtn = $('<button></button>').html('<i class="fas fa-check"></i>');
+        $completeBtn.addClass("complete-btn")
+        $todoDiv.append($completeBtn);
+        const $trashBtn = $('<button></button>').html('<i class="fas fa-trash"></i>');
+        $trashBtn.addClass("trash-btn")
+        $todoDiv.append($trashBtn);
+        $todoDiv.append(JSON.stringify(data[i].name))
+        $todoList.append($todoDiv);
+        $todoInput.val("");
+    }
+}
+
+const addTodo = (event) => {
+    event.preventDefault();
+    const newTodo = {
+        "name": $todoInput.val(),
+        "list_id": 1
+    }
+    fetch("/todo", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newTodo)
+        })
+        .then(res => res.json())
+        .then(data => {
+            createTodo(data)
+        });
+};
+
+const getTodo = (event) => {
+
+    fetch("/todo")
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            createTodo(data)
+        })
+
+}
+
+
+const deleteCheck = (e) => {
+    const item = e.target;
+    const deleteItem =
+        fetch("/todo", {
+            method: 'DELETE',
+            headers: {
+                "Content-type": "application/json"
+            }
+        })
+
+    if (item.classList[0] === 'trash-btn') {
+        const todo = item.parentElement;
+        todo.classList.add('fall');
+        todo.addEventListener('transitioned', () => {
+            todo.remove();
+            deleteItem(todo);
+        })
+
+    }
+
+    if (item.classList[0] === "complete-btn") {
+        const todo = item.parentElement;
+        todo.classList.toggle('completed');
+    }
+}
+
+function filterTodo(e) {
+    const todos = e.childElement;
+    for (let e = 0; e < todos.length; i++) {
+        switch (e.target.value) {
+            case "all":
+                todos.style.display = "flex";
+                break;
+            case "completed":
+                if (todos.classList.contains("completed")) {
+                    todos.style.display = "flex";
+                } else {
+                    todos.style.display = "none";
+                }
+                break;
+            case "uncompleted":
+                if (!todos.classList.contains('completed')) {
+                    todos.style.display = "flex";
+                } else {
+                    todos.style.display = "none";
+                }
+                break;
+        }
+    };
+}
+
+//event listeners
+$todoBtn.on('click', addTodo)
+$todoList.on('click', deleteCheck)
+$filterOption.on('click', filterTodo)
+
+//called function
+getTodo();
